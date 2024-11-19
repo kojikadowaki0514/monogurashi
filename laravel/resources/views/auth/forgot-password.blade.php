0dcd44c2d14fd01fe27162e3e6b8062570a64c4d
kojikadowake0514 <gbsg9405@gmail.com>
2024-11-19 09:25:22 +0900

    パスワード再設定用の画面作成

--- laravel/resources/views/auth/forgot-password.blade.php
+++ laravel/resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="mb-2 text-sm text-red-600 text-center py-4 px-6">
+        {{ __('登録時のパスワードを入力してください') }}
     </div>
 
     <!-- Session Status -->
@@ -17,7 +17,7 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <x-primary-button style="background-color:#0098ad">
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
